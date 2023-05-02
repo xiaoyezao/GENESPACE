2aa8de49bfa9f2870dd9f211e8400d1b74667a40
xiaoyezao <44076439+xiaoyezao@users.noreply.github.com>
2023-05-02 22:36:47 +0200

    Update annotate_bed.R

--- R/annotate_bed.R
+++ R/annotate_bed.R
@@ -147,7 +147,7 @@ annotate_bed <- function(gsParam){
   nGaps       <- gsParam$params$nGaps
   synBuff     <- gsParam$params$synBuff
   blkSize     <- gsParam$params$blkSize
-  maxOgPlaces <- gsParam$ploidy * 8
+  maxOgPlaces <- gsParam$params$maxOgPlaces
   arrayJump   <- gsParam$params$synBuff / 2
   nCores      <- gsParam$params$nCores
   ploidy      <- gsParam$ploidy
